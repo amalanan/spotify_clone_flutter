fbd38ef7756a460e9314478f8f88f82ba6cd2a64
Amal Anan <133678416+amalanan@users.noreply.github.com>
2025-07-27 20:29:18 +0300

    Update splash.dart

--- lib/presentation/splash/pages/splash.dart
+++ lib/presentation/splash/pages/splash.dart
@@ -1,1 +1,12 @@
+import 'package:flutter/material.dart';
 
+class SplashPage extends StatelessWidget {
+  const SplashPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(child: Image.asset('assets/vectors/spotify_logo.svg')),
+    );
+  }
+}
